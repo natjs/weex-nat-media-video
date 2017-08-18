85b63bb05cef5219da871a1baf9150564d864c99
Acathur <chiahungzhang@gmail.com>
2017-08-18 19:13:57 +0800

    0.2.3 released

--- android/Video.java
+++ android/Video.java
@@ -1,21 +1,21 @@
 package com.nat.weex;
 
-import com.nat.media_video.HLModuleResultListener;
-import com.nat.media_video.HLVideoModule;
+import com.nat.media_video.ModuleResultListener;
+import com.nat.media_video.VideoModule;
 import com.taobao.weex.annotation.JSMethod;
 import com.taobao.weex.bridge.JSCallback;
 import com.taobao.weex.common.WXModule;
 
 /**
- * Created by Daniel on 17/2/17.
- * Copyright (c) 2017 Nat. All rights reserved.
+ * Created by Acathur on 17/2/17.
+ * Copyright (c) 2017 Instapp. All rights reserved.
  */
 
-public class MediaVideoModule extends WXModule {
+public class Video extends WXModule {
 
     @JSMethod
     public void play(String path, final JSCallback jsCallback) {
-        HLVideoModule.getInstance(mWXSDKInstance.getContext()).play(path, new HLModuleResultListener() {
+        VideoModule.getInstance(mWXSDKInstance.getContext()).play(path, new ModuleResultListener() {
             @Override
             public void onResult(Object o) {
                 jsCallback.invoke(o);
@@ -25,7 +25,7 @@ public class MediaVideoModule extends WXModule {
 
     @JSMethod
     public void pause(final JSCallback jsCallback) {
-        HLVideoModule.getInstance(mWXSDKInstance.getContext()).pause(new HLModuleResultListener() {
+        VideoModule.getInstance(mWXSDKInstance.getContext()).pause(new ModuleResultListener() {
             @Override
             public void onResult(Object o) {
                 jsCallback.invoke(o);
@@ -35,7 +35,7 @@ public class MediaVideoModule extends WXModule {
 
     @JSMethod
     public void stop(final JSCallback jsCallback) {
-        HLVideoModule.getInstance(mWXSDKInstance.getContext()).stop(new HLModuleResultListener() {
+        VideoModule.getInstance(mWXSDKInstance.getContext()).stop(new ModuleResultListener() {
             @Override
             public void onResult(Object o) {
                 jsCallback.invoke(o);
--- ios/WeexNatVideo.m
+++ ios/WeexNatVideo.m
@@ -2,13 +2,14 @@
 //  WeexNatVideo.m
 //
 //  Created by huangyake on 17/1/7.
-//  Copyright © 2017 Nat. All rights reserved.
+//  Copyright © 2017 Instapp. All rights reserved.
 //
 
 #import "WeexNatVideo.h"
 #import "NatVideo.h"
 
 @implementation WeexNatVideo
+@synthesize weexInstance;
 WX_EXPORT_METHOD(@selector(play::))
 WX_EXPORT_METHOD(@selector(pause:))
 WX_EXPORT_METHOD(@selector(stop:))
